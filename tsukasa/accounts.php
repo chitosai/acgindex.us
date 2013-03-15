a7d24ff1c304d791057aea5d9bb9eaf679769cab
TheC <i@thec.me>
2013-03-15 20:22:02 +0800

    clear account info

--- tsukasa/accounts.php
+++ tsukasa/accounts.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_USER', 'root');
-define('DB_PASS', '111000');
-define('DB_NAME', 'acgindex');
-define('DB_HOST', '127.0.0.1');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+define('DB_HOST', '');
